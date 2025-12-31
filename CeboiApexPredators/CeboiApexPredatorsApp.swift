5a5da8cc39282ebb30fdd27659043802608e84f5
Mahaldi WIjaya Saputra <aldiwijayasaputra@gmail.com>
2025-12-31 20:08:06 +0700

    Initial Commit

---
+++ CeboiApexPredators/CeboiApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeboiApexPredatorsApp.swift
+//  CeboiApexPredators
+//
+//  Created by ceboi on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CeboiApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
